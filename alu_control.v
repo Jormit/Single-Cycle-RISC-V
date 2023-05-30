18b159442f2260e4f2f0b26c0f48e6c44bbad449
Jordan Mitchell <z5257117@ad.unsw.edu.au>
2023-05-30 20:50:59 +1000

    Fix dependency warning in alu control

--- alu_control.v
+++ alu_control.v
@@ -13,7 +13,7 @@ module alu_control (
 	reg [3:0] op_funct;
 	reg [3:0] op_imm_funct;
 
-	always @(*) begin
+	always @(alu_op, branch_funct, op_funct, op_imm_funct) begin
 		case (alu_op)
 			`CTL_ALU_ADD:    operation <= `ALU_ADD;
 			`CTL_ALU_BRANCH: operation <= branch_funct;
@@ -21,17 +21,21 @@ module alu_control (
 			`CTL_ALU_OP_IMM: operation <= op_imm_funct;
 			default:         branch_funct <= 4'bx;
 		endcase
-		
+	end
+	
+	always @(funct3) begin
 		case (funct3)
 			`FUNCT3_BRANCH_EQ:	branch_funct <= `ALU_EQ;
 			`FUNCT3_BRANCH_NE:	branch_funct <= `ALU_EQ;
-			`ALU_LT:         	branch_funct <= `ALU_LT;
+			`FUNCT3_BRANCH_LT:  branch_funct <= `ALU_LT;
 			`FUNCT3_BRANCH_GE:	branch_funct <= `ALU_LT;
 			`FUNCT3_BRANCH_LTU:	branch_funct <= `ALU_LTU;
 			`FUNCT3_BRANCH_GEU:	branch_funct <= `ALU_LTU;
 			default:            branch_funct <= 4'bx;
 		endcase
+	end
 
+	always @(funct3) begin
 		case (funct3)
 			`FUNCT3_ALU_ADD_SUB: primary_op_funct <= `ALU_ADD;
 			`FUNCT3_ALU_SL:      primary_op_funct <= `ALU_SLL;
@@ -43,14 +47,17 @@ module alu_control (
 			`FUNCT3_ALU_AND:     primary_op_funct <= `ALU_AND;	
 			default:             primary_op_funct <= 4'bx;		
 		endcase
+	end
 
+	always @(funct3) begin
 		case (funct3)
 			`FUNCT3_ALU_ADD_SUB: secondary_op_funct <= `ALU_SUB;
 			`FUNCT3_ALU_SR:      secondary_op_funct <= `ALU_SRA;
 			default:             secondary_op_funct <= 4'bx;		
 		endcase
+	end
 
-
+	always @(funct7, secondary_op_funct, primary_op_funct) begin
 		if (funct7[5]) op_funct = secondary_op_funct;
 		else           op_funct = primary_op_funct;
 
